d755bb904c744cdadbeeaa860110d0daae021463
Muhammad Nabil <nabilsabtu1154@gmail.com>
2025-09-03 08:57:44 +0800

    contact/footer change

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -52,14 +52,17 @@
         <div class="w-full flex justify-center mb-8">
             <div class="flex gap-4 mb-4">
                 <a href="#" class="flex items-center justify-center w-12 h-12 rounded-full bg-teal-100 text-teal-600 hover:bg-teal-200 transition" title="Instagram">
+                    <!-- Instagram Official SVG -->
                     <svg width="24" height="24" viewBox="0 0 448 512" fill="currentColor"><path d="M224.1 141c-63.6 0-115.1 51.5-115.1 115.1S160.5 371.3 224.1 371.3 339.2 319.8 339.2 256.1 287.7 141 224.1 141zm0 186.6c-39.6 0-71.7-32.1-71.7-71.7s32.1-71.7 71.7-71.7 71.7 32.1 71.7 71.7-32.1 71.7-71.7 71.7zm146.4-194.3c0 14.9-12.1 27-27 27s-27-12.1-27-27 12.1-27 27-27 27 12.1 27 27zm76.1 27.2c-1.7-35.3-9.9-66.7-36.2-92.9S388.6 9.7 353.3 8c-35.3-1.7-138.6-1.7-173.9 0-35.3 1.7-66.7 9.9-92.9 36.2S9.7 123.4 8 158.7c-1.7 35.3-1.7 138.6 0 173.9 1.7 35.3 9.9 66.7 36.2 92.9s57.6 34.5 92.9 36.2c35.3 1.7 138.6 1.7 173.9 0 35.3-1.7 66.7-9.9 92.9-36.2s34.5-57.6 36.2-92.9c1.7-35.3 1.7-138.6 0-173.9zM398.8 388c-7.8 19.6-22.9 34.7-42.5 42.5-29.5 11.7-99.5 9-132.3 9s-102.7 2.6-132.3-9c-19.6-7.8-34.7-22.9-42.5-42.5-11.7-29.5-9-99.5-9-132.3s-2.6-102.7 9-132.3c7.8-19.6 22.9-34.7 42.5-42.5 29.5-11.7 99.5-9 132.3-9s102.7-2.6 132.3 9c19.6 7.8 34.7 22.9 42.5 42.5 11.7 29.5 9 99.5 9 132.3s2.6 102.7-9 132.3z"/></svg>
                 </a>
                 <a href="#" class="flex items-center justify-center w-12 h-12 rounded-full bg-teal-100 text-teal-600 hover:bg-teal-200 transition" title="Facebook">
+                    <!-- Facebook Official SVG -->
                     <svg width="24" height="24" viewBox="0 0 320 512" fill="currentColor"><path d="M279.14 288l14.22-92.66h-88.91V127.89c0-25.35 12.42-50.06 52.24-50.06H293V6.26S259.5 0 225.36 0c-73.22 0-121.36 44.38-121.36 124.72V195.3H22.89V288h81.11v224h100.2V288z"/></svg>
                 </a>
                 <a href="#" class="flex items-center justify-center w-12 h-12 rounded-full bg-teal-100 text-teal-600 hover:bg-teal-200 transition" title="X (FKA Twitter)">
-                    <svg width="24" height="24" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M909.5 327H789.5L600 573.5L410.5 327H290.5L540.5 661.5L290.5 900H410.5L600 684.5L789.5 900H909.5L659.5 661.5L909.5 327Z" fill="#0d9488"/>
+                    <!-- X Official SVG: exact path, themed color -->
+                    <svg width="24" height="24" viewBox="0 0 512 512" fill="none">
+                        <path d="M437.252 80H338.748L256 196.252 173.252 80H74.748L208.252 256 74.748 432H173.252L256 315.748 338.748 432h98.504L303.748 256 437.252 80z" fill="#0d9488"/>
                     </svg>
                 </a>
             </div>
